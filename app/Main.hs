5a36807f2102707c369acd6cab003437d19990d7
JayDepp <jjaydepp@yahoo.com>
2017-08-09 20:59:36 -0400

    Use forM_ and iterate instead of custom loop

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,19 @@
 module Main where
 
 import Conway
-import Control.Concurrent
+import Control.Concurrent (threadDelay)
+import Control.Monad (forM_)
 
 tickTime :: Int
 tickTime = 200000
 
-start :: Grid Bool
+start :: Grid
 start = mkGrid $
      glider `at` (0, 0)
   ++ beacon `at` (15, 5)
 
 main :: IO ()
-main = loop (step basicRule) start
-
-loop :: (Grid Bool -> Grid Bool) -> Grid Bool -> IO ()
-loop stepper g = do
+main = forM_ (iterate (step basicRule) start) $ \grid -> do
   putStr "\ESC[2J" -- Clear terminal screen
-  putStrLn (render g)
+  putStrLn (render grid)
   threadDelay tickTime
-  loop stepper (stepper g)
